32f66e57270c77dd50857030f415e7449c91acf8
Blackdrag01 <51270741+Blackdrag01@users.noreply.github.com>
2023-11-29 15:32:27 -0300

    Add files via upload

--- JogoTCC/index.php
+++ JogoTCC/index.php
@@ -6,77 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RPG de Browser</title>
     <link rel="stylesheet" href="styles.css">
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f5f5f5;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
-
-        .container {
-            background-color: #fff;
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            padding: 20px;
-            width: 300px;
-        }
-
-        h1 {
-            text-align: center;
-            color: #333;
-        }
-
-        .game-container {
-            margin-top: 20px;
-        }
-
-        .row {
-            margin-bottom: 15px;
-        }
-
-        label {
-            display: block;
-            margin-bottom: 5px;
-            color: #555;
-        }
-
-        input {
-            width: 100%;
-            padding: 8px;
-            margin-bottom: 10px;
-            box-sizing: border-box;
-        }
-
-        button {
-            background-color: #4caf50;
-            color: #fff;
-            padding: 10px;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
-        }
-
-        button:hover {
-            background-color: #45a049;
-        }
-
-        a {
-            text-align: center;
-            display: block;
-            margin-top: 15px;
-            color: #333;
-            text-decoration: none;
-        }
-
-        a:hover {
-            text-decoration: underline;
-        }
-    </style>
+    
 </head>
 
 <body>
@@ -90,7 +20,7 @@
                     <label for="senha">Senha</label>
                     <input type="password" name="senha" id="senha" required>
                     <div class="col">
-                        <button type="submit">Login</button>
+                        <button type="submit" align = "center">Login</button>
                     </div>
                 </form>
             </div>
